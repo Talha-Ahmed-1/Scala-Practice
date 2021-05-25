ed2f2f2e9fb6a8aca4033e681bb2d5a755adc20a
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-05-25 19:06:50 +0500

    1 and 2 added

--- src/Assignment3/Excercise1.scala
+++ src/Assignment3/Excercise1.scala
@@ -4,7 +4,19 @@ object Excercise1 {
   val a = 3
   val b = 5
   val c = 7
-  val lst = List(-3,-2,-1,0,1,2,3)
+
+  def loop(from:Int,to:Int):List[Int] = {
+    def go(n:Int,n1:Int, res:List[Int]):List[Int] = {
+      if (n == n1+1 ){
+        res
+      }else{
+        go(n+1,n1, res :+ n)
+      }
+    }
+    return go(from,to,List())
+  }
+
+  val lst = loop(-3,3)
 
   def y(x:Int) = a*(x*x) + b*x + c
 
--- src/Assignment3/Excercise2.scala
+++ src/Assignment3/Excercise2.scala
@@ -1,6 +1,19 @@
 package Assignment3
+import Excercise1._
 
 object Excercise2 {
+  val lst1 = loop(-3,3)
+  val lst2 = lst1.map(y _)
 
+  val lst3 = lst1.zip(lst2).zipWithIndex map {case ((a, b), c) => List(a, b, c)}
+
+  val sum1 = lst3.map(_(1)).reduce(_+_)
+  val sum2 = lst3.map(_(2)).reduce(_ max _)+1
+  val mean = sum1/sum2
+
+  def main(args:Array[String]): Unit = {
+    println(lst3)
+    println(s"Mean = ${mean}")
+  }
 
 }
