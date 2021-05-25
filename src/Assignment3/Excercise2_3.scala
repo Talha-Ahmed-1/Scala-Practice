b17e34de5d17fcaff86b252acbbb126de82c4f5f
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-05-25 23:50:01 +0500

    1_3 updated

---
+++ src/Assignment3/Excercise2_3.scala
@@ -0,0 +1,9 @@
+package Assignment3
+
+object Excercise2_3 {
+  def func(arg1: Any, arg2: Any):String = arg1.toString + arg2.toString
+
+  def main(args:Array[String]): Unit = {
+    println(func(12,43))
+  }
+}
